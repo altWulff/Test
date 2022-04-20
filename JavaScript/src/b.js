3090587d5560b349e854925c2e81b4526a34cede
Oleg Repalo <altwulf7@gmail.com>
2022-04-20 09:54:19 +0300

    Update b.js

--- JavaScript/src/b.js
+++ JavaScript/src/b.js
@@ -1,9 +1,11 @@
 function printInformation() {
-    const roomName = document.querySelector('.bui-list__item').innerText;
-    const priceDetails = document.getElementsByClassName('bp-price-details__charge-value e2e-price-details__total-charge--user');
-    const span = priceDetails[0].getElementsByTagName('span');
-    const roundPrice = span[0].getAttribute('data-value');
-    const currency = span[0].getAttribute('data-currency');
-
-    console.log('Room name: '+roomName+'. Currency: '+currency+'. Price: '+roundPrice)
+    var roomName = document.querySelector('.bui-list__item').innerText;
+	const allPrice = document.getElementsByClassName("bp-price-details__charge-value e2e-price-details__total-charge--user")[0].innerText;
+	var currencyISOCode = allPrice.split(' ')[0].slice(0, 3);
+	var detailPrice = allPrice.split(' ')[0].slice(4);
+	var selectText = document.getElementsByClassName("bui-f-font-strong")[1].innerText;
+	var selectPriceTetx = document.getElementsByClassName("bp-price-details__charge-name")[1].innerText;
+    console.log(selectText +' '+roomName);
+	console.log(selectPriceTetx + ': '+currencyISOCode+' '+detailPrice+'.');
 };
+     
