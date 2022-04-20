16cfca453e380c61dce4a3b73da4df3f908d7c80
Oleg Repalo <altwulf7@gmail.com>
2022-04-20 13:36:38 +0300

    Update a.js

--- JavaScript/src/a.js
+++ JavaScript/src/a.js
@@ -1,5 +1,44 @@
+function requesInfo(indx) {	
+	var pers  = document.getElementsByClassName('hprt-table-cell hprt-table-cell-occupancy')[indx]
+	var getCls = pers.nextElementSibling.classList;
+	var roomPrice = document.getElementsByClassName(getCls[0])[indx].getElementsByClassName('prco-valign-middle-helper')[0].innerText;
+	var ocupacy = document.getElementsByClassName('hprt-table-cell hprt-table-cell-occupancy')[indx].getElementsByClassName('hprt-block')[0].innerText;
+	var seectId = document.getElementsByClassName(' hprt-table-cell hprt-table-room-select')[indx].getElementsByTagName('select')[0].id;
+	return Array(String('#'+seectId), parseInt(ocupacy.split(':')[1]), parseFloat(roomPrice.split(' ')[0].slice(4).replace(',', '.')));
+};
+
+
+function getArrayMin(array) {
+	return Math.min.apply(null, array);
+};
+
+
+function findMinWithMaxPersons(array, maxPersons) {
+	var newArray = [];
+	for (var i = 0; i < array.length; i++) {
+		if (requesInfo(i)[1] == maxPersons) {
+			newArray.push(requesInfo(i)[2]);
+		};	
+	};
+	return getArrayMin(newArray);
+};
+
+
+function getIndexFromArray(array, number) {
+	for (var i = 0; i < array.length; i++) {
+		if (requesInfo(i)[2] == number) {
+			return i;
+		};
+	};	
+};
+
+
 function addButtonEvent() {
-    var select = document.querySelector('#hprt_nos_select_32934401_211007526_0_1_0'),
+	var table = document.getElementsByClassName('hprt-table-cell hprt-table-cell-occupancy');
+	var chepeastRoom = findMinWithMaxPersons(table, 2);
+	var arrayIndex = getIndexFromArray(table, chepeastRoom); 
+	var selectId = requesInfo(arrayIndex)[0];
+    var select = document.querySelector(selectId),
         input = document.getElementsByClassName('txp-bui-main-pp bui-button bui-button--primary  hp_rt_input js-reservation-button px--fw-cta');
 
     if (document.getElementById('b_tt_holder_1') == null ) {
